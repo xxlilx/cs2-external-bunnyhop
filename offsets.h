de3d8ecf4836f3cf181add5783c86725e5020051
xxlilx <124320724+xxlilx@users.noreply.github.com>
2024-03-20 17:19:44 +0300

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -9,7 +9,7 @@ namespace Offsets
 {
 	adr64 client = cs2.GetModuleBaseAddress(L"client.dll");
 
-	const uintptr_t dwForceJump = 0x17294A0;//bhop
+	const uintptr_t dwForceJump = 0x1730530;//bhop
 }
 
 #endif
